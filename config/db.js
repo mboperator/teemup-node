05f6bf1df2bdc55b403a015736d5124ea9bbdb96
Marcus Bernales <mboperator@gmail.com>
2014-09-28 13:16:38 -0700

    Fixed db adapter

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 // config/db.js
-  module.exports = {
-    url: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/teemup';
-  }
+var url = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/teemup';
+module.exports = {
+  url: url
+}
